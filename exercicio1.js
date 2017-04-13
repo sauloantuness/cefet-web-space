47fb8b8f5e77ea5597263d120d857b355074bcb4
Saulo Antunes <sauloantuness@gmail.com>
2017-04-12 21:55:56 -0300

    Prática

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,16 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+
+document.querySelectorAll('.botao-expandir-retrair').forEach(function(button){
+	button.addEventListener('click', function(event){
+		let p = button.closest('p');
+		if(p.classList.contains('expandido')) {
+			p.classList.remove('expandido');
+			button.innerHTML = '+';
+		}
+		else {
+			p.classList.add('expandido');
+			button.innerHTML = '-';
+		}
+	});
+});
--- exercicio2.js
+++ exercicio2.js
@@ -2,10 +2,27 @@
 // Este arquivo ainda não está incluído no arquivo HTML
 
 const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
-  todasAsImagens = [
-    'philae-parts.jpg',
-    'philae-rosetta.jpg',
-    'philae-separation.jpg',
-    'philae-67-picture.jpg',
-    'philae-collecting.jpg'
-  ];
+      todasAsImagens = [
+        'philae-parts.jpg',
+        'philae-rosetta.jpg',
+        'philae-separation.jpg',
+        'philae-67-picture.jpg',
+        'philae-collecting.jpg'
+      ];
+
+var currentImageIndex = 0;
+
+document.querySelector('#anterior').addEventListener('click', function(event){
+    currentImageIndex = (currentImageIndex + (todasAsImagens.length - 1)) % todasAsImagens.length;
+    changeImage();
+});
+
+document.querySelector('#proximo').addEventListener('click', function(event){
+    currentImageIndex = (currentImageIndex + 1) % todasAsImagens.length;
+    changeImage();
+});
+
+var changeImage = function(){
+    let img = document.querySelector('#galeria > img');
+    img.src = servidorDasImagens + todasAsImagens[currentImageIndex];
+};
